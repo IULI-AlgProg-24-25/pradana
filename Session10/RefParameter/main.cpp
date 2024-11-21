d7f39d663a7bc126be1a699a8dd60f7052dc3f21
pat85 <pradana.a@gmail.com>
2024-11-21 14:23:07 +0700

    value & ref type

---
+++ Session10/RefParameter/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+void testFunction(int& num) // shallow copy
+{
+    cout << "original num inside the function: " << num << endl;
+
+    num = 10;
+
+    cout << "updated num inside the function: " << num << endl;    
+}
+
+int main()
+{
+    int x = 6;
+
+    cout << "value x before function: " << x << endl;
+
+    testFunction(x);
+
+    cout << "value x after function: " << x << endl;
+
+    return 0;
+}
